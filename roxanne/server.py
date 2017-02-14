2f2e49a0c22b17b110a7e6fcbeca959b40a9eec3
Jacques Troussard <jtroussa@mail.umw.edu>
2017-02-14 23:46:39 +0000

    fixed navigation bar highlighting

--- roxanne/server.py
+++ roxanne/server.py
@@ -3,26 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def mainIndex():
-	return render_template('index.html')
+	return render_template('index.html', selected='home')
 	
 @app.route('/about')
 def mainAbout():
-    return render_template('about.html')
+    return render_template('about.html', selected='about')
     
 @app.route('/blog')
 def mainBlog():
-    return render_template('blog.html')
+    return render_template('blog.html', selected='blog')
     
 @app.route('/meet')
 def mainMeet():
     showMonth = "April"
     isPresent = False;
     shows = {'date': 'April 6-9', 'location': 'The Charlotte Motor Speedway', 'address': '5555 Concord Pkwy S, Concord, NC 28027', 'name': 'Charlotte AutoFair', 'description': 'These events provide collector car Flea Market Vendor spaces to buy and sell restoration parts and supplies for almost any vehicle ever produced - in addition to Car Corral vehicle spaces on the track oval for buying and selling collector vehicles of all descriptions! The collector car Flea Market includes everything automotive, including memorabilia, vintage signs, tires, wheels, automotive toys, restoration supplies, tools, and classic cars for sale!'}
-    return render_template('meet.html', showMonth=showMonth, show=shows, isPres=isPresent)
+    return render_template('meet.html', selected='meet', showMonth=showMonth, show=shows, isPres=isPresent)
 
 @app.route('/form')
 def mainForm():
-    return render_template('form.html')
+    return render_template('form.html', selected='form')
     
 @app.route('/vids')
 def mainVids():
@@ -31,7 +31,7 @@ def mainVids():
                 {'title': 'Nice Example of Restored GTO', 'link': 'HT7bKCTxUhM?start=9'},
                 {'title': 'Paul Cangialosi explains Gear Ratios', 'link': 'RyOWKW21I0c'}]
                 
-    return render_template('vids.html', vids=videos)
+    return render_template('vids.html', selected='vids', vids=videos)
 # start the server
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8080, debug=True)
